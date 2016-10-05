b735a289ca876a80489f9c876c9b6f751bea6324
Caroline Ham <cyham@wccnet.edu>
2016-10-04 23:34:34 -0400

    Update Drivetrain.java
    I finished putting what we already had into the command based programming that FIRST suggests us to use.

--- src/org/usfirst/frc/team6101/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team6101/robot/subsystems/Drivetrain.java
@@ -14,11 +14,53 @@ public class DriveTrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	public Encoder rEncoder = new Encoder(RobotMap.EncoderMap.RIGHT_ENCODER_FIRST_PORT, RobotMap.EncoderMap.RIGHT_ENCODER_SECOND_PORT, false, Encoder.EncodingType.k4X); 
-	public Encoder lEncoder = new Encoder(RobotMap.EncoderMap.LEFT_ENCODER_FIRST_PORT, RobotMap.EncoderMap.RIGHT_ENCODER_SECOND_PORT, false, Encoder.EncodingType.k4X); 
-	
-    public void initDefaultCommand() {
+	public Encoder lEncoder = new Encoder(RobotMap.EncoderMap.LEFT_ENCODER_FIRST_PORT, RobotMap.EncoderMap.RIGHT_ENCODER_SECOND_PORT, false, Encoder.EncodingType.k4X);
+	public long rDriftm, lDrift;
+    
+	public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-    }
-}
+	}
+	
+	public void resetEncoders(){
+		rEncoder.reset();
+		lEncoder.reset();
+	}
+	
+	public void startEncoders(){
+		rEncoder.start();
+		lEncoder.start();
+	}
+	
+	public void stopEncoders(){
+		rEncoder.stop();
+		lEncoder.stop();
+	}
+	
+	public int[] getRevolutions(){
+		// Way to calculate revolutions
+		int [] revCounts = new int[2];
+		int lRevs = // number of revoutions;
+		int rRevs = // number of revolutions;	
+		revCounts[0] = lRevs;
+		revCounts[1] = rRevs;
+		return (revCounts);
+	}
+	
+	public void slowRight(lRevs, rRevs){
+		rDrift = lRevs/rRevs;
+		lDrift = 1;
+	}
+	
+	public void slowLeft(lRevs, rRevs){
+		rDrift = 1;
+		lDrift = rRevs/lRevs;
+	}
+	
+	public void motorSame(){
+		rDrift = 1;
+		lDrift = 1;
+	}
+	
+	
 
