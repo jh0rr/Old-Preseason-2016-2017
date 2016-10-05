bfa0858c3fe3fa8ab9242ba27b231e2266eecce2
Caroline Ham <cyham@wccnet.edu>
2016-10-04 22:37:07 -0400

    Update Robot.java

--- src/org/usfirst/frc/team6101/robot/Robot.java
+++ src/org/usfirst/frc/team6101/robot/Robot.java
@@ -4,8 +4,8 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import org.usfirst.frc.team6101.robot.commands.ExampleCommand;
-import org.usfirst.frc.team6101.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team6101.robot.commands.*;
+import org.usfirst.frc.team6101.robot.subsystems.*;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -19,6 +19,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends IterativeRobot {
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static final Drivetrain driveTrain = new Drivetrain();
 	public static OI oi;
 
     Command autonomousCommand;
