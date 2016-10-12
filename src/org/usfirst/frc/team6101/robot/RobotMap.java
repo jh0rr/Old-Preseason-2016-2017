6c301800438561dc0acfdb7bc1662ab11846b900
Caroline Ham <cyham@wccnet.edu>
2016-10-11 20:31:52 -0400

    Update RobotMap.java

--- src/org/usfirst/frc/team6101/robot/RobotMap.java
+++ src/org/usfirst/frc/team6101/robot/RobotMap.java
@@ -16,7 +16,18 @@ public class RobotMap {
         public static final int LEFT_ENCODER_FIRST_PORT = 2;
         public static final int LEFT_ENCODER_SECOND_PORT = 3;
     }
-
+    public class MotorMap{
+    	public static final int TOP_LEFT_MOTOR_POWER = 0;
+    	public static final int TOP_RIGHT_MOTOR_POWER = 1;
+    	public static final int BOTTOM_LEFT_MOTOR_POWER = 2;
+    	public static final int BOTTOM_RIGHT_MOTOR_POWER = 3;
+    }
+    public class GyroMap{
+    	public static final int TOP_LEFT_GYRO = 0;
+    	public static final int TOP_RIGHT_GYRO = 1;
+    	public static final int BOTTOM_LEFT_GYRO = 2;
+    	public static final int BOTTOM_RIGHT_GYRO = 3;
+    }
 
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
